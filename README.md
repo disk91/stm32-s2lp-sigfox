288b7887c1f5f3c63ee8087b1d1ebb1b9977039b
Paul Pinault <disk_91@hotmail.com>
2018-11-03 10:32:23 +0100

    Initial commmit - blink a led

---
+++ README.md
@@ -0,0 +1,6 @@
+This project is a demo project on how to configure a STM32L053R8 board with a S2LP StMicroelectronic solution for transmitting over Sigfox Network.
+
+The project is used as a documentation for supporting a blog post. Check this blog post to understand how to use it.
+See here : https://www.disk91.com/?p=3388
+
+The project have different tag for the different step of realization.
---
+++ Release/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_rtc.d
@@ -0,0 +1,107 @@
+Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_rtc.o: \
+ ../Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_rtc.c \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Core/Inc/stm32l0xx_hal_conf.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Core/Inc/main.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Include/core_cm0plus.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc_ex.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_iwdg.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rtc.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rtc_ex.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_spi.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim_ex.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h \
+ C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Core/Inc/stm32l0xx_hal_conf.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Core/Inc/main.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Include/core_cm0plus.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc_ex.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_iwdg.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rtc.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rtc_ex.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_spi.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim_ex.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h:
+
+C:/Users/paul/Documents/STM32/STM32_Sigfox_ItSdk/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h:
